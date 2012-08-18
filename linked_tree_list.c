9052ca0575e2bd2d9ceb426696e7ba134f038f8b
Katherine Threlkeld <kathrelkeld@gmail.com>
2012-08-18 15:55:38 -0700

    Graphviz dot files for results

--- linked_tree_list.c
+++ linked_tree_list.c
@@ -17,34 +17,40 @@ typedef struct {
 } pair;
 
 void bst_to_list_helper(node *n, pair *p_parent) {
-    if (n == NULL) {
-        return;
+    /* LEFT  */
+        /* if NULL leaf, connect parent's left to self and self to left of parent */
+    if (n->left == NULL) {
+        n->left = p_parent->left;
+        p_parent->left = n;
+    } else {
+        pair *p_toleft = (pair *)malloc(sizeof(pair));
+        p_toleft->left = p_parent->left;
+        p_toleft->right = n;
+        /* modify p_toleft with child's values */
+        bst_to_list_helper(n->left, p_toleft);
+        p_parent->left = p_toleft->left;
+        n->left = p_toleft->right;
+        free(p_toleft);
     }
 
-    /* LEFT  */
-    pair *p_toleft = (pair *)malloc(sizeof(pair));
-    p_toleft->left = p_parent->left;
-    p_toleft->right = n;
-    /* modify p_toleft with child's values */
-    bst_to_list_helper(n->left, p_toleft);
-    p_parent->left = p_toleft->left;
-    n->left = p_toleft->right;
-    free(p_toleft);
-
     /* RIGHT */
-    pair *p_toright = (pair *)malloc(sizeof(pair));
-    p_toright->right = p_parent->right;
-    p_toright->left = n;
-    /* modify p_toright with child's values */
-    bst_to_list_helper(n->right, p_toright);
-    p_parent->right = p_toright->right;
-    n->right = p_toright->left;
-    free(p_toright);
+        /* if NULL leaf, connect parent's right to self and self to right of parent */
+    if (n->right == NULL) {
+        n->right = p_parent->right;
+        p_parent->right = n;
+    } else {
+        pair *p_toright = (pair *)malloc(sizeof(pair));
+        p_toright->right = p_parent->right;
+        p_toright->left = n;
+        /* modify p_toright with child's values */
+        bst_to_list_helper(n->right, p_toright);
+        p_parent->right = p_toright->right;
+        n->right = p_toright->left;
+        free(p_toright);
+    }
     
     return;
 }
-
-
 node * bst_to_list(node *bst) {
     if (bst == NULL) {
         return bst;
@@ -55,10 +61,12 @@ node * bst_to_list(node *bst) {
     bst_to_list_helper(bst, p);
     (p->left)->left = p->right;
     (p->right)->right = p->left;
-    return p->left;
+    node *result = p->left;
+    free(p);
+    return result;
 }
 
-/* Creates a random bst from an array of sorted integers */
+/* creates a random bst from an array of sorted integers */
 node * bst_create(int input[], int len) {
     if (len == 0) {
         return NULL;
@@ -219,24 +227,140 @@ void bst_print(node *bst) {
     ll_destroy(next_row);
 }
 
-/* Prints the given circular list */
-void circular_list_print(node *list) {
+/* One-line version of bst_print */
+void bst_print_oneline_helper(node *bst, bool is_rightmost) {
+    if (bst == NULL) {
+        return;
+    }
+    if (bst->left != NULL) {
+        bst_print_oneline_helper(bst->left, false);
+    }
+    if (is_rightmost && (bst->right == NULL)) {
+        printf("%d\n", bst->value);
+    } else {
+        printf("%d,", bst->value);
+    }
+    if (bst->right != NULL) {
+        bst_print_oneline_helper(bst->right, is_rightmost);
+    }
+    return;
+}
+void bst_print_oneline(node *bst) {
+    bst_print_oneline_helper(bst, true);
+}
 
+/* Creates graphviz compatible dotfile format of given bst */
+void graphviz_print_bst_helper(node *bst, FILE *fp) {
+    if (bst->left) {
+        fprintf(fp, "%d -> %d;\n", bst->value, bst->left->value);
+        graphviz_print_bst_helper(bst->left, fp);
+    }
+    if (bst->right) {
+        fprintf(fp, "%d -> %d;\n", bst->value, bst->right->value);
+        graphviz_print_bst_helper(bst->right, fp);
+    }
+    return;
+}
+void graphviz_print_bst(node *bst) {
+    FILE *fp = fopen("bst.dot", "w");
+    if (!fp) {
+        printf("ERROR: Could not open graphviz file for write");
+        return;
+    }
+    fprintf(fp, "digraph bst {\n");
+    if (bst) {
+        graphviz_print_bst_helper(bst, fp);
+    }
+    fprintf(fp, "}\n");
+    fclose(fp);
+}
+
+/* Prints the given circular list and verifies that it is circular */
+void circular_list_print_helper(node *current, node *head, node *prev) {
+    /* Check that links for current node match what previous has */
+    if ((current == NULL) || (prev != current->left)) {
+        printf("\nError found in element of circular list\n");
+        return;
+    }
+    printf(",%d", current->value);
+    /* Verify that the next node is not where we started */
+    if (current->right != head) {
+        circular_list_print_helper(current->right, head, current);
+    }
+    return;
+}
+void circular_list_print(node *list) {
+    if (list == NULL){
+        printf("\n");
+        return;
+    }
+    printf("%d", list->value);
+    if (list->right != list) {
+        circular_list_print_helper(list->right, list, list);
+        /* Check the one remaining link pair matches */
+        if (list->left->right != list) {
+            printf("\nError found in first element of circular list\n");
+        }
+    }
+    printf("\n");
+    return;
+}
+
+void graphviz_print_circular_list(node *list) {
+    FILE *fp = fopen("list.dot", "w");
+    if (!fp) {
+        printf("ERROR: Could not open graphviz file for write");
+        return;
+    }
+    fprintf(fp, "digraph list {\n");
+    if (list) {
+        node *current = list;
+        while (current->right != list){
+            fprintf(fp, "%d -> %d\n", current->value, current->right->value);
+            fprintf(fp, "%d -> %d\n", current->value, current->left->value);
+            current = current->right;
+        }
+        fprintf(fp, "%d -> %d\n", current->value, current->right->value);
+        fprintf(fp, "%d -> %d\n", current->value, current->left->value);
+    }
+    fprintf(fp, "}\n");
+    fclose(fp);
+    return;
+}
+
+/* Destroy the given circular list */
+void circular_list_destroy(node *list) {
+    if (!list) {
+        return;
+    }
+    list->left->right = NULL;
+    while (list) {
+        node *next = list->right;
+        free(list);
+        list = next;
+    }
 }
 
 int main() {
     srand(time(NULL));
 
     /* Create an of value 1 to N where N is randomly chosen from 0...10 */
-    int input_size = rand() % 11;
+    int input_size = rand() % 11 + 5;
     int *test_input = (int *)malloc(input_size * sizeof(int));
     for (int i = 0; i < input_size; ++i) {
         test_input[i] = i + 1;
     }
     node *bst = bst_create(test_input, input_size);
     free(test_input);
-    bst_print(bst);
-    bst_destroy(bst);
+    /*bst_print(bst);*/
+    graphviz_print_bst(bst);
+    system("dot bst.dot -Tpng -o bst.png");
+    system("xdg-open bst.png");
+    bst = bst_to_list(bst);
+    graphviz_print_circular_list(bst);
+    system("circo list.dot -Tpng -o list.png");
+    system("xdg-open list.png");
+    circular_list_destroy(bst);
 
     return 0;
 }
